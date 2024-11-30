4fb4249b22074482b4a14984bf8a5beaa63d2136
vskySW-2024 <prasad.sawant@vskysolutions.com>
2024-11-30 11:48:39 +0530

    commented text summarizer

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, request, redirect, url_for, Blueprint, jsonify
 from app.models.page_setup_model import page_setup_model
 from app.models.text_summarizer_model import text_summarizer_model
-import app.utils.text_summeriser as ts
+#import app.utils.text_summeriser as ts
 
 bp = Blueprint('main', __name__)
 
@@ -18,24 +18,24 @@ def text_summarizer_documentation():
 
     return render_template('text_summarizer_dashboard.html',pages_setup_model=pages_setup_model)
 
-@bp.route('/text_summarizer/summariser', methods=['GET','POST'])
-def text_summarizer_summariser():
-    #page setup
-    pages_setup_model = page_setup_model()
-    pages_setup_model.page_path = ["Text Summariser", "Text Summariser"]
-    pages_setup_model.page_html_id = "text-summariser-page-2"
+# @bp.route('/text_summarizer/summariser', methods=['GET','POST'])
+# def text_summarizer_summariser():
+#     #page setup
+#     pages_setup_model = page_setup_model()
+#     pages_setup_model.page_path = ["Text Summariser", "Text Summariser"]
+#     pages_setup_model.page_html_id = "text-summariser-page-2"
 
-    o_text_summarizer_model = text_summarizer_model()
-    o_text_summarizer_model.input_text = ''
-    if request.method == 'POST':
-        input_text = request.form.get('inputText').strip()
-        if not input_text:
-            o_text_summarizer_model.error_message = "Please enter proper text."
-            return render_template('text_summarizer_summariser.html',pages_setup_model=pages_setup_model,page_model=o_text_summarizer_model)
-        o_text_summarizer_model.input_text = input_text
-        o_text_summarizer_model.output_text = ts.summarize_text(input_text)
+#     o_text_summarizer_model = text_summarizer_model()
+#     o_text_summarizer_model.input_text = ''
+#     if request.method == 'POST':
+#         input_text = request.form.get('inputText').strip()
+#         if not input_text:
+#             o_text_summarizer_model.error_message = "Please enter proper text."
+#             return render_template('text_summarizer_summariser.html',pages_setup_model=pages_setup_model,page_model=o_text_summarizer_model)
+#         o_text_summarizer_model.input_text = input_text
+#         o_text_summarizer_model.output_text = ts.summarize_text(input_text)
 
-    return render_template('text_summarizer_summariser.html',pages_setup_model=pages_setup_model,page_model=o_text_summarizer_model)
+#     return render_template('text_summarizer_summariser.html',pages_setup_model=pages_setup_model,page_model=o_text_summarizer_model)
 
 @bp.route('/chatbots')
 def chatbots_documentation():
--- app/utils/text_summeriser.py
+++ app/utils/text_summeriser.py
@@ -1,12 +1,12 @@
-from transformers import pipeline
+# from transformers import pipeline
 
-# Initialize the summarizer pipeline outside the function for efficiency
-summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+# # Initialize the summarizer pipeline outside the function for efficiency
+# summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 
-def summarize_text(input_text):
-    if len(input_text.split()) > 10:
-        summary = summarizer(input_text, max_length=50, min_length=20, do_sample=False)
-        return summary[0]['summary_text']
-    else:
-        return "The input text is too short to summarize. Text need at least 20 words."
+# def summarize_text(input_text):
+#     if len(input_text.split()) > 10:
+#         summary = summarizer(input_text, max_length=50, min_length=20, do_sample=False)
+#         return summary[0]['summary_text']
+#     else:
+#         return "The input text is too short to summarize. Text need at least 20 words."
 
